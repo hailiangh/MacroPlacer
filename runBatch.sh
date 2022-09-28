c7e02f58056f05f2f640766fdcc028480a5f3c25
Hailiang Hu <hailiang@ecesvj10101.ece.tamu.edu>
2022-09-27 19:21:18 -0500

    add some batches

---
+++ runBatch.sh
@@ -0,0 +1,4 @@
+./main1 --batch input/job_4.batch | tee input/job_4.log
+./main1 --batch input/job_5.batch | tee input/job_5.log
+./main1 --batch input/job_6.batch | tee input/job_6.log
+./main1 --batch input/job_7.batch | tee input/job_7.log
