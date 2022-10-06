d97db950cdf6e04b67c4790f066323fdfbad1332
Ubuntu20_04 <hailiang@tamu.edu>
2022-10-06 16:25:26 -0500

    update batch

--- runBatch.sh
+++ runBatch.sh
@@ -1,2 +1,2 @@
-./main1 --batch input/job_6.batch | tee input/job_6.log
-./main1 --batch input/job_8.batch | tee input/job_8.log
+./main --batch input/job_6.batch | tee input/job_6.log
+./main --batch input/job_8.batch | tee input/job_8.log
